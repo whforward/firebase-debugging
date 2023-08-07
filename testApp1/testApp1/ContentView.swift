c1ba7cbd622fc1ed327b0fded5a92eacd9dfce42
Phillip Gorrindo <pgorrindo@users.noreply.github.com>
2023-08-07 10:46:58 -0700

    Initial commit

---
+++ testApp1/testApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testApp1
+//
+//  Created by Phil Gorrindo on 2023-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testApp1/testApp1/testApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  testApp1App.swift
+//  testApp1
+//
+//  Created by Phil Gorrindo on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct testApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
