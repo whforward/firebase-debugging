77958408098f8f68f03eb355289254a7728d2c01
Phillip Gorrindo <pgorrindo@users.noreply.github.com>
2023-10-30 20:08:13 -0700

    add testApp2

---
+++ testApp2/testApp2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  testApp2
+//
+//  Created by Phil Gorrindo on 2023-10-30.
+//
+
+import Firebase 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testApp2/testApp2/testApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  testApp2App.swift
+//  testApp2
+//
+//  Created by Phil Gorrindo on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct testApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
